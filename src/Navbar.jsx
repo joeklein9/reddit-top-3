d9ba98b44e38c7195a0611278a73cf17b1a440b6
Joseph Kleinhenz <115609619+joeklein9@users.noreply.github.com>
2023-03-13 11:13:26 -0400

    moved icon images

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar () {
     return (
     <>
         <navbar className = "navbar-container">
-            <img className = "navbar-icon" src = "/public/static/img/white-reddit-icon.svg"/>
+            <img className = "navbar-icon" src = "/src/static/white-reddit-icon.svg"/>
             <h3 className = "navbar-text">Top 5 Posts of All Time by Subreddit</h3>
         </navbar>
     </>
